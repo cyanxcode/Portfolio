fc219176e0f7f9584917dd476f0b4ed61a79f903
Cyan <codinginstant@example.com>
2025-05-15 19:03:01 +0530

    Work Base

--- src/lib/components/Work.svelte
+++ src/lib/components/Work.svelte
@@ -1,23 +1,18 @@
-<h1 class="karantina text-right py-10 px-20 text-white text-5xl md:text-7xl lg:text-8xl leading-tight uppercase whitespace-nowrap">
+<script>
+	import WorkItems from "./WorkItems.svelte";
+
+</script>
+<h1 class="karantina text-right py-10 px-4 sm:px-8 md:px-20 text-white text-4xl sm:text-5xl md:text-7xl lg:text-8xl leading-tight uppercase">
     My Work
-</h1>
-<div class="grid grid-cols-1 md:grid-cols-2 p-6 gap-4 gap-y-20 jetbrains">
-    
-    <div class="w-full flex flex-col gap-4">
-        <img src="/img/cursor/Apple.png" alt=""  class="w-full aspect-video">
-        <div class="text-zinc-500 flex gap-8 pl-10 text-sm"> <p>React JS</p><p>•</p><p>GSAP</p><p>•</p><p>Tailwindcss</p></div>
-        <div class="text-white flex gap-4 hover:gap-8 hover:px-10 px-12 items-center text-lg transition-tall duration-100 ease-linear">
-            <img src="/extra/arrow-right.svg" alt="" class="w-10">
-            Apple Website Recreated
-        </div>
-    </div>
-    <div class="w-full flex flex-col gap-4">
-        <img src="/img/cursor/Amprio.png" alt=""  class="w-full aspect-video">
-        <div class="text-zinc-500 flex gap-8 pl-10 text-sm"> <p>Next JS</p><p>•</p><p>Shopify</p><p>•</p><p>Tailwindcss</p></div>
-        <div class="text-white flex gap-4 hover:gap-8 hover:px-10 px-12 items-center text-lg transition-tall duration-100 ease-linear">
-            <img src="/extra/arrow-right.svg" alt="" class="w-10">
-            Amprio: Lighting Brand
-        </div>
-    </div>
-</div>
+  </h1>
+  
+  <div class="grid grid-cols-1 md:grid-cols-3 px-4 sm:px-8 md:px-20 gap-4">
+    <WorkItems type="tall" img="/work/Apple2.png" title="Apple Website Recreated" description="Redesigned Apple Website showcasing iPhone 15 Pro"/>    
+    <WorkItems type="square" img="/work/Apple.png" title="Apple Website Recreated" description="Redesigned Apple Website showcasing iPhone 15 Pro"/>
+    <WorkItems type="square" img="/work/Cybercup.png" title="Cybercup" description="Designed the Website for National Level Hackathon"/>
+    <WorkItems type="wide" img="/work/Amprio.png" title="Amprio" description="A full-fledged ecommerce store for a lighting brand"/>
+
+
+  
+  </div>
 <button class="my-10 flex items-center justify-center gap-3 px-5 py-2 rounded-full font-bold bg-white  ml-[50%] -translate-x-1/2">Show all <img src="/extra/arrow-black-r.svg" class="w-8" alt=""></button>
---
+++ src/lib/components/WorkItems.svelte
@@ -0,0 +1,35 @@
+<script>
+    let {type= "square", img, title, description, link = ""} = $props();
+    let isOpen = $state(false);
+    switch (type) {
+        case "square":
+            type= "aspect-square"
+            break
+        case "tall":
+            type= "md:row-span-2"
+            break
+        case "wide":
+            type= "md:col-span-2"
+            break
+        default:
+            type= "aspect-square"
+    }
+    function handleClick() {
+        if (!isOpen) {
+            isOpen = true;
+        } else {
+            // Go to the link
+        }
+    }
+</script>
+
+
+<button class={`${type} w-full h-full overflow-hidden rounded-2xl object-cover relative group`} onclick={handleClick}>
+    <img src={img} alt={title} class={`w-full h-full object-cover md:blur-md md:group-hover:blur-none ${isOpen ? "blur-none" : 'blur-md'} transition-all duration-300 ease-in-out`}>
+    <div class={`absolute w-full h-full top-0 left-0 z-20 bg-gradient-to-t p-5 from-black md:opacity-0 md:group-hover:opacity-100 ${isOpen ? "opacity-100" : "opacity-0"} transition-all duration-300 ease-in-out`}>
+      <div class="flex flex-col items-start justify-end h-full gap-4">
+        <h4 class="text-xl sm:text-2xl md:text-3xl karantina text-white">{title}</h4>
+        <p class="text-zinc-300 text-sm ml-2">{description}</p>
+      </div>
+    </div>
+</button>
