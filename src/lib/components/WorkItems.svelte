0747fc01d67807bce7ef634ebc950321a943eff1
Cyan <codinginstant@example.com>
2025-05-15 20:34:50 +0530

    slightly fixed work

--- src/lib/components/WorkItems.svelte
+++ src/lib/components/WorkItems.svelte
@@ -27,9 +27,9 @@
 <button class={`${type} w-full h-full overflow-hidden rounded-2xl object-cover relative group`} onclick={handleClick}>
     <img src={img} alt={title} class={`w-full h-full object-cover md:blur-md md:group-hover:blur-none ${isOpen ? "blur-none" : 'blur-md'} transition-all duration-300 ease-in-out`}>
     <div class={`absolute w-full h-full top-0 left-0 z-20 bg-gradient-to-t p-5 from-black md:opacity-0 md:group-hover:opacity-100 ${isOpen ? "opacity-100" : "opacity-0"} transition-all duration-300 ease-in-out`}>
-      <div class="flex flex-col items-start justify-end h-full gap-4">
-        <h4 class="text-xl sm:text-2xl md:text-3xl karantina text-white">{title}</h4>
-        <p class="text-zinc-300 text-sm ml-2">{description}</p>
+      <div class="flex flex-col items-start justify-end h-full gap-2 text-left ">
+        <h4 class="text-2xl tracking-wider lg:text-3xl karantina text-white whitespace-nowrap">{title}</h4>
+        <p class="text-zinc-300 text-sm ml-2 truncate whitespace-nowrap w-[90%]">{description}</p>
       </div>
     </div>
 </button>
