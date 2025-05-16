8aae1cc728e2ad8756de606ea85197a320543678
Cyan <codinginstant@example.com>
2025-05-16 11:25:23 +0530

    changed color for small screen testimonials

--- src/lib/components/Testimonial.svelte
+++ src/lib/components/Testimonial.svelte
@@ -18,21 +18,20 @@
 <h1 class="karantina text-left py-10 px-4 sm:px-8 md:px-20 text-white text-4xl sm:text-5xl md:text-7xl lg:text-8xl leading-tight uppercase">
     Testimonials
 </h1>
-
 <div class="flex flex-col items-center w-full space-y-6 px-4 sm:px-8 md:px-20">
     {#each testimonials as testimonial}
-    <div class="w-full bg-black text-white hover:bg-white hover:text-black transition-colors duration-300 group rounded-xl p-4 sm:p-6 flex flex-col md:flex-row md:items-center gap-4">
+    <div class="w-full bg-white text-black md:bg-black md:text-white md:hover:bg-white md:hover:text-black transition-colors duration-300 group rounded-xl p-4 sm:p-6 flex flex-col md:flex-row md:items-center gap-4">
         
         <!-- Profile Section -->
         <div class="flex md:w-[40%] w-full items-center gap-4">
-            <div class="w-14 h-14 sm:w-16 sm:h-16 rounded-full bg-white group-hover:bg-black shrink-0"></div>
+            <div class="w-14 h-14 sm:w-16 sm:h-16 rounded-full bg-black md:bg-white md:group-hover:bg-black shrink-0"></div>
             <div class="flex flex-col">
                 <p class="text-lg sm:text-xl font-medium leading-tight">{testimonial.name}</p>
                 {#if testimonial.title}
-                    <p class="text-sm sm:text-base text-zinc-300 group-hover:text-zinc-600">{testimonial.title}</p>
+                    <p class="text-sm sm:text-base text-zinc-600 md:text-zinc-300 md:group-hover:text-zinc-600">{testimonial.title}</p>
                 {/if}
                 {#if testimonial.company}
-                    <p class="text-sm sm:text-base text-zinc-300 group-hover:text-zinc-600">{testimonial.company}</p>
+                    <p class="text-sm sm:text-base text-zinc-600 md:text-zinc-300 md:group-hover:text-zinc-600">{testimonial.company}</p>
                 {/if}
             </div>
         </div>
@@ -45,7 +44,6 @@
 
     <!-- Divider -->
     {#if !(testimonials[testimonials.length - 1] === testimonial)}
-
     <div class="w-full h-[1px] bg-zinc-300"></div>
     {/if}
     {/each}
